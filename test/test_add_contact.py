bbf1c52cd4a83cd1d6cccd152da7f905053bb6a6
RomanRyabushko <roman.1950@mail.ru>
2024-09-04 22:57:22 +0300

    Реализована генерация и загрузка тестовых данных

--- test/test_add_contact.py
+++ test/test_add_contact.py
@@ -1,24 +1,8 @@
 from model.contact import Contact
-import random
-import string
-import pytest
 
 
-def random_string(prefix, maxlen):
-    symbols = (string.ascii_letters + string.digits
-               #+ string.punctuation
-               + " "*10)
-    return prefix + "".join([random.choice(symbols) for i in range(random.randrange(maxlen))])
-
-
-testdata = [Contact(firstname="", lastname="", address="")] + [
-    Contact(firstname=random_string("firstname", 10), lastname=random_string("lastname", 20),
-            address=random_string("address", 10))
-    for i in range(5)]
-
-
-@pytest.mark.parametrize("contact", testdata, ids=[repr(x) for x in testdata])
-def test_add_contact(app, contact):
+def test_add_contact(app, json_contacts):
+    contact = json_contacts
     old_contacts = app.contact.get_contact_list()
     app.contact.create(contact)
     assert len(old_contacts) + 1 == app.contact.count()
